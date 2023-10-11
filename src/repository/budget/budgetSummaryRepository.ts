11ea3e914b556fd69bccc0f8d2c27eb46d7af689
Guide Promwat <promwat@me.com>
2023-10-11 14:00:22 +0200

    exercise 2

--- src/repository/budget/budgetSummaryRepository.ts
+++ src/repository/budget/budgetSummaryRepository.ts
@@ -2,7 +2,9 @@ import { type BudgetId, type BudgetSummary } from "../../domain/budget";
 
 interface BudgetSummaryRepository {
   // TODO 2.2 - add a function "insert" for inserting a new BudgetSummary
+  insert: (budgetSummary: BudgetSummary) => Promise<BudgetSummary>
   find: (budgetId: BudgetId) => Promise<BudgetSummary | undefined>;
+  
 }
 
 export default BudgetSummaryRepository;
--- src/repository/budget/mongo/budgetSummaryMongoRepository.ts
+++ src/repository/budget/mongo/budgetSummaryMongoRepository.ts
@@ -1,7 +1,8 @@
 import type BudgetSummaryRepository from "../budgetSummaryRepository";
 import { BudgetSummaryEntityConverter } from "../entity/converters";
-import { type BudgetId } from "../../../domain/budget";
+import { BudgetSummary, type BudgetId } from "../../../domain/budget";
 import { BudgetSummaryModel } from "./models";
+import BudgetSummaryEntity from "../entity/budgetSummaryEntity";
 
 // TODO 2.3 - implement the new "insertBudgetSummary" function
 
@@ -14,9 +15,19 @@ export const findBudgetSummary: BudgetSummaryRepository["find"] = async (
   }
 };
 
+export const insertBudgetSummary: BudgetSummaryRepository["insert"] = async (
+  budgetSummary: BudgetSummary,
+) => {
+  const entity: BudgetSummaryEntity = BudgetSummaryEntityConverter.toEntity(budgetSummary);
+  const insertedSummary: BudgetSummaryEntity = await new BudgetSummaryModel(entity).save();
+  const insertedBudgetSummary: BudgetSummary = BudgetSummaryEntityConverter.toDomain(insertedSummary);
+  return insertedBudgetSummary;
+};
+
 const BudgetSummaryMongoRepository = (): BudgetSummaryRepository => ({
   // TODO 2.4. - add the "insertBudgetSummary" function to the repository
   find: findBudgetSummary,
+  insert: insertBudgetSummary
 });
 
 export default BudgetSummaryMongoRepository;
